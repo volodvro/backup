98585b350865be1fe798a6afe817228226e053bb
volodvro <volodvro@gmail.com>
2018-10-30 23:51:19 +0200

    asus wifi driver for ubuntu

---
+++ asusAc53nano/8822bu.mod.c
@@ -0,0 +1,33 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=cfg80211";
+
+MODULE_ALIAS("usb:v0BDApB812d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v0B05p1812d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v7392pB822d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v0B05p184Cd*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v0BDApB82Cd*dc*dsc*dp*icFFiscFFipFFin*");
+
+MODULE_INFO(srcversion, "5CA92F92BF11FC216E41AFD");
